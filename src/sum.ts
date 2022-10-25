bd8c38e8c661e60e69ba39934a4a854c027479a9
Harvey <harvey0379@163.com>
2022-10-25 23:33:41 +0800

    chore: jsdoc

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,10 @@
-export default function sum(a: number, b: number): number {
+import { Total } from './types'
+
+/**
+ * @param  {number} a
+ * @param  {number} b
+ * @returns Total
+ */
+export default function sum(a: number, b: number): Total {
   return a + b
 }
